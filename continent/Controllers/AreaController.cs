f837ad192bc4a5a1ac3847d17b4a077d1b6d2dae
MYounas <younus.ubit56@gmail.com>
2017-01-22 12:43:44 +0530

    crud done

--- continent/Controllers/AreaController.cs
+++ continent/Controllers/AreaController.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity.Migrations;
+using System.Data.Entity;
 
 namespace continent.Controllers
 {
@@ -19,9 +21,7 @@ namespace continent.Controllers
         public ActionResult Create()
         {
             ViewBag.CO_id = new SelectList(db.countries, "id", "name");
-            //ViewBag.CI_id = new SelectList(db.cities, "id", "name");
             ViewBag.CI_id = new SelectList(new List<string>(), "id", "name");
-            //ViewBag.CI_id = new SelectList();
             return View();
         }
 
@@ -39,7 +39,33 @@ namespace continent.Controllers
             return RedirectToAction("Index");
         }
 
+        public ActionResult Edit(int id)
+        {
+            var area = db.areas.Find(id);
+            var co_id = db.cities.Find(area.CI_id).CO_id;
 
+            var cities = db.cities.Where(x => x.CO_id == co_id);
+
+            ViewBag.CO_id = new SelectList(db.countries, "id", "name",co_id);
+            ViewBag.CI_id = new SelectList(cities, "id", "name", area.CI_id);
+            return View(area);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(area a)
+        {
+            db.areas.AddOrUpdate(a);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var area = db.areas.Find(id);
+            db.areas.Remove(area);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
 
     }
 }
